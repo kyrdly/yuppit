54e11023036fb94827d3366683a93c03b81a0fe4
Kyle Ridley <kyrdly@gmail.com>
2013-11-25 17:02:00 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yupp::Application.config.secret_key_base = '3747418a20e0e1780ac82e431479c9a8bb71eac79323d724402fd38f91a9d4e6ad5820c2b4cb7ee226fb86acccd61b3019d41ef0d3a460a26c9309c34ba54420'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yupp::Application.config.session_store :cookie_store, key: '_Yupp_session'
